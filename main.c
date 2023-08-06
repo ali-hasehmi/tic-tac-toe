d30fea41d82dd0f03b6ef9ae24f1689ecf115687
ali-hashemi <AGHashemi.1382@gmail.com>
2023-08-07 01:15:17 +0330

    compatibility with windows

--- main.c
+++ main.c
@@ -1,113 +1,181 @@
 #include <stdio.h>
 #include <stdlib.h>
-	
-#define OR  ||
+
+#include <conio.h>
+#define OR ||
 #define AND &&
 
-	char board[9];
-	void board_init();
-	void board_print(int curr);
-	void board_clear();
-	void clear_screen();
-	int main(int argc, char *argv[]){
+char board[9];
+int curr = 0;
+void board_init();
+void board_print();
+void board_clear();
+void board_getinput();
+void clear_screen();
+int main(int argc, char *argv[])
+{
 
-		board_init();
+	board_init();
 
-		int index = 0;	
-		board_print(index);
-		char input;
-		while((input=getchar())!='4'){
-			if(input == '\033')
-			{
-				getchar();
-				switch(getchar())
-				{
-					case 'A': //up
-						if(index!=0 AND index!=1 AND  index !=2) {
-							index -= 3;
-						}	
-						break;
-					case 'B': //down
-						  if(index != 6 AND index != 7 AND index != 8){
-							  index +=3;
-						  }
-						  break;
-					case 'C': //right
-						  if(index != 2 AND index != 5 AND index != 8)
-						  {
-							  index++;
-						  }
-						  break;
-					case 'D': //left
-						  if(index != 0 AND index != 3 AND index != 6){
-							  index--;
-						  }
-						  break;
-				}
-				board_print(index);
-				printf("index = %d \n",index);
-			}
-		}
-		getchar();
+	board_print();
 
-	return 0; 
+	board_getinput();
+
+	getchar();
+
+	return 0;
 }
-void board_init(){
+void board_init()
+{
 
-	#ifdef _WIN32
-		// if os is windows(either 64 or 32) these lines will be executed
-		system("chcp 65001");
-	#endif
-	
-	#ifdef __linux__
-		// if os is linux these lines will be executed
-		printf("this program is running on linux operating system!\n");
-	#endif
+#ifdef _WIN32
+	// if os is windows(either 64 or 32) these lines will be executed
+	system("chcp 65001");
+#endif
+
+#ifdef __linux__
+	// if os is linux these lines will be executed
+	printf("this program is running on linux operating system!\n");
+#endif
 	board_clear();
-	}
-	void board_print(int curr){
-		
-		clear_screen();
-		size_t index = 0;
-		for( size_t i = 0 ; i < 5 ; ++i){
-			for( size_t j = 0 ; j < 17 ; ++j){
-				
-				if(i == 1 OR i == 3){
-					if(j % 6 == 5){
-						printf("\u253c");
-					}
-					else{
-						printf("\u2500");
-					}
-				}	
-				else{
-					if(j % 6 == 5){
-						printf("\u2502");
-					}
-					else if(j % 6 == 2){
-						if(index == curr){
-							printf("\033[31;107;4m%c\033[0m",board[index++]);
-						}
-						else{
-							printf("%c",board[index++]);
-						}	
+}
+void board_print()
+{
 
-					}
-					else{
-						printf(" ");
-					}
+	clear_screen();
+	size_t index = 0;
+	for (size_t i = 0; i < 5; ++i)
+	{
+		for (size_t j = 0; j < 17; ++j)
+		{
+
+			if (i == 1 OR i == 3)
+			{
+				if (j % 6 == 5)
+				{
+					printf("\u253c");
+				}
+				else
+				{
+					printf("\u2500");
 				}
 			}
-			printf("\n");
-	       	}
-	}
-
-	void clear_screen(){
-		system("cls || clear");
-	}
-
-	void board_clear(){
-		for( size_t i = 0 ; i < 9 ; ++i){
-			board[i] = 32;
+			else
+			{
+				if (j % 6 == 5)
+				{
+					printf("\u2502");
+				}
+				else if (j % 6 == 2)
+				{
+					if (index == curr)
+					{
+						printf("\033[31;107;4m%c\033[0m", board[index++]);
+					}
+					else
+					{
+						printf("%c", board[index++]);
+					}
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
 		}
+		printf("\n");
 	}
+}
+
+void clear_screen()
+{
+	system("cls || clear");
+}
+
+void board_clear()
+{
+	for (size_t i = 0; i < 9; ++i)
+	{
+		board[i] = 32;
+	}
+}
+
+void board_getinput()
+{
+	unsigned char input;
+	while ((input = getch()) != '4')
+	{
+
+#ifdef _WIN32
+
+		if (input == 0xE0) // to check scroll key interrupt
+		{
+
+			switch (input = getch())
+			{
+			case 72: // up
+				if (curr != 0 AND curr != 1 AND curr != 2)
+				{
+					curr -= 3;
+				}
+				break;
+			case 80: // down
+				if (curr != 6 AND curr != 7 AND curr != 8)
+				{
+					curr += 3;
+				}
+				break;
+			case 75: // left
+				if (curr != 0 AND curr != 3 AND curr != 6)
+				{
+					curr--;
+				}
+				break;
+			case 77: // right
+				if (curr != 2 AND curr != 5 AND curr != 8)
+				{
+					curr++;
+				}
+				break;
+			};
+		}
+
+#endif
+
+#ifdef __linux__
+		if (input == '\033')
+		{
+			getchar();
+			switch ((input = getchar()))
+			{
+			case 'A': // up
+				if (curr != 0 AND curr != 1 AND curr != 2)
+				{
+					curr -= 3;
+				}
+				break;
+			case 'B': // down
+				if (curr != 6 AND curr != 7 AND curr != 8)
+				{
+					curr += 3;
+				}
+				break;
+			case 'C': // right
+				if (curr != 2 AND curr != 5 AND curr != 8)
+				{
+					curr++;
+				}
+				break;
+			case 'D': // left
+				if (curr != 0 AND curr != 3 AND curr != 6)
+				{
+					curr--;
+				}
+				break;
+			}
+		}
+#endif
+		board_print();
+		printf("index = %d \n", curr);
+	}
+}
